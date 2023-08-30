c724313be1b38ea6a3822157e1edc844c84f08fa
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-30 22:29:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,26 +15,8 @@ const corsOptions = {
 app.use(cors(corsOptions)); 
 
 db()
-app.use(express.json());
-app.use("/auth",auth)
-app.use("/tip",tip)
-const express = require("express"),
-      app = express(),
-      db = require("./db")
-const auth = require("./routes/auth"),
-      tip = require("./routes/tip")
-const cors = require('cors');
 
 
-// CORS ayarlarını özelleştirme
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,POST',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions)); 
-
-db()
 app.use(express.json());
 app.use("/auth",auth)
 app.use("/tip",tip)
